10bceaa9bb858e58f3e096afd4fd53e81b858df7
kuusei <motaro@kuusei.moe>
2021-12-14 20:53:49 +0800

    feat: 初始化bot

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# HYkomo-bot
+# HYkumo-bot
+基于nonebot的bot项目,设计用于泛acg资源管理和常规bot功能.
+
+## How to start
+
+1. generate project using `nb create` .
+2. writing your plugins under `src/plugins` folder.
+3. run your bot using `nb run` .
---
+++ bot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import nonebot
+# 使用cqhttp插件
+from nonebot.adapters.cqhttp import Bot as CQHTTPBot
+# 日志插件
+from nonebot.log import logger, default_format
+
+# 初始化日志
+logger.add("error.log",
+           rotation="00:00",
+           diagnose=False,
+           level="ERROR",
+           format=default_format)
+
+nonebot.init()
+# 如果在 bot 入口文件内定义了 asgi server， nb-cli 将会为你启动冷重载模式（当文件发生变动时自动重启 NoneBot 实例）
+app = nonebot.get_asgi()
+
+# 加载 NoneBot 内置的 CQHTTP 协议适配组件
+driver = nonebot.get_driver()
+driver.register_adapter("cqhttp", CQHTTPBot)
+
+nonebot.load_builtin_plugins()
+nonebot.load_from_toml("pyproject.toml")
+
+if __name__ == "__main__":
+    nonebot.logger.warning("Always use `nb run` to start the bot instead of manually running!")
+    nonebot.run(app="__mp_main__:app")
